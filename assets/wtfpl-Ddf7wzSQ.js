e8ff64ad0d94e7b62c0e1554e34ecf6272f4885c
WickyPlays <nguyenthienbao8601@gmail.com>
2025-06-27 12:54:41 +0700

    Updates

---
+++ assets/wtfpl-Ddf7wzSQ.js
@@ -0,0 +1,15 @@
+const n=`            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE\r
+                    Version 2, December 2004\r
+\r
+ Copyright (C) {{ year }} {{ organization }}\r
+\r
+ Everyone is permitted to copy and distribute verbatim or modified\r
+ copies of this license document, and changing it is allowed as long\r
+ as the name is changed.\r
+\r
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE\r
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION\r
+\r
+  0. You just DO WHAT THE FUCK YOU WANT TO.\r
+\r
+`;export{n as default};
